04f21bcf2f2cbfd4ed56d809e6716899203d744f
Lionel ADANKON <lionelad006@gmail.com>
2023-05-08 17:21:23 +0100

    Blog page and article CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Blog from "./pages/Blog";
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
     {/* Le path * est utilisé lorsque l'url demandé n'est pas déclaré ou est inexsistant */}
         <Route path="*" element={<Home />} /> 
       </Routes>
